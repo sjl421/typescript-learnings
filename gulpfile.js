ef203778230f36130848cde1792efc74e93cc3d0
gnu4cn <unisko@gmail.com>
2017-12-01 11:44:46 +0800

    Browserify added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 const gulp = require('gulp'),
-      ts = require('gulp-typescript'),
-      tsProject = ts.createProject('tsconfig.json');
+      browserify = require('browserify'),
+      source = require('vinyl-source-stream'),
+      tsify = require('tsify'),
+      paths = {
+        pages: ['src/*.html']
+      };
 
-gulp.task("default", ()=>{
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"))
+gulp.task("copy-html", ()=>{
+    return gulp.src(path.pages)
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task('defaults', ['copy-html'], ()=>{
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pip(gulp.dest('dist'));
 });
